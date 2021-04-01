1aa370239896c099fa7bf9f929005c16dbc420d4
annahyan <anna@menchelab.com>
2021-04-01 20:55:04 +0200

    Added points argument, which makes the heatmap a bubble plot

--- R/ggheatmap.R
+++ R/ggheatmap.R
@@ -13,8 +13,11 @@
 #' @examples ggheatmap(mtcars)
 #' @importFrom magrittr %>%
 #' @export 
-ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0.5, 
-    fontSize = 20, colorPalette = "Spectral", scaleName = "value", distMethod = "euclidean", 
+ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, points = F,
+                      dendroLineSize = 0.5, 
+                      fontSize = 20,
+                      colorPalette = "Spectral",
+                      scaleName = "value", distMethod = "euclidean", 
     clustMethod = "complete", revColors=F) {
     
     data_m <- tibble::rownames_to_column(dataMatrix) %>% reshape2::melt()
@@ -36,23 +39,39 @@ ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0
         data_m$variable <- factor(data_m$variable, levels = ordered_col_names)
     }
     
-    heat_plot <- ggplot2::ggplot(data_m, ggplot2::aes(x = variable, y = rowname, fill = value)) + 
-                     ggplot2::geom_tile() + 
-		     ggplot2::theme_minimal() + 
+    heat_plot <- ggplot2::ggplot(data_m, ggplot2::aes(x = variable, y = rowname))
+
+    if (points == TRUE) {
+        heat_plot = heat_plot +
+            ggplot2::geom_point(aes(color = value, size = abs(value) ) ) +
+            ggplot2::scale_color_distiller(palette = colorPalette, name = scaleName,
+                                           direction=ifelse(revColors,0,1)) +
+            guides(size = "none")
+    } else {
+        heat_plot = heat_plot +
+            ggplot2::geom_tile(aes(fill = value) ) +
+            ggplot2::scale_fill_distiller(palette = colorPalette, name = scaleName,
+                                          direction=ifelse(revColors,0,1))
+    }
+
+    heat_plot = heat_plot +  
+                     ggplot2::theme_minimal() + 
 		     ggplot2::theme(axis.line = ggplot2::element_line(size = 0),
 		         text = ggplot2::element_text(size = fontSize),
-			 axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5)) + 
+			 axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5),
+                         panel.border = element_rect(color = "gray20", fill = NA) ) + 
                      ggplot2::scale_y_discrete(position = "right") + 
                      ggplot2::xlab("") + 
-		     ggplot2::ylab("") + 
-		     ggplot2::scale_fill_distiller(palette = colorPalette, name = scaleName, direction=ifelse(revColors,0,1))
+		     ggplot2::ylab("")
+		     
     
     final_plot <- heat_plot
 
     if (orderRow) {
         dendro_data_row <- ggdendro::dendro_data(dd.row, type = "rectangle")
         dendro_row <- cowplot::axis_canvas(heat_plot, axis = "y", coord_flip = TRUE) + 
-                          ggplot2::geom_segment(data = ggdendro::segment(dendro_data_row), ggplot2::aes(y = -y, 
+            ggplot2::geom_segment(data = ggdendro::segment(dendro_data_row),
+                                  ggplot2::aes(y = -y, 
 	                  x = x, xend = xend, yend = -yend), size = dendroLineSize) + 
                           ggplot2::coord_flip()
         final_plot <- cowplot::insert_yaxis_grob(final_plot, dendro_row, grid::unit(0.2, 
@@ -62,7 +81,8 @@ ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0
     if (orderCol) {
         dendro_data_col <- ggdendro::dendro_data(dd.col, type = "rectangle")
         dendro_col <- cowplot::axis_canvas(heat_plot, axis = "x") + 
-		      ggplot2::geom_segment(data = ggdendro::segment(dendro_data_col), ggplot2::aes(x = x, y = y, xend = xend, yend = yend), size = dendroLineSize)
+            ggplot2::geom_segment(data = ggdendro::segment(dendro_data_col),
+                                  ggplot2::aes(x = x, y = y, xend = xend, yend = yend), size = dendroLineSize)
         final_plot <- cowplot::insert_xaxis_grob(final_plot, dendro_col, grid::unit(0.2, 
                           "null"), position = "top")
     }
